df4449807ab511d785a992a678b693e0a23106b1
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-02-27 02:01:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    
+    w = word.split("").sort
+    
+    # array_sort = array.map{|w| w.split("").sort}.reject {|e| e!=w} 
+    
+    array.select{|e| e.split("").sort == w } #--> 
+    
+    # array_sort = array.each{|item| item.match}
+      
+    #   array_sort
+    # else
+      
+    # end
+  end
+  
+end
